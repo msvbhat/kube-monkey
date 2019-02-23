12abd180f2866f95975d7ff9fe30c1701ee3e30e
M S Vishwanath Bhat <msvbhat@gmail.com>
2019-02-23 12:53:05 +0100

    Get a list of pods which are eligible to be deleted
    This patch expects a env variable NAMESPACES_WHITELIST which
    should contain space sepeterated list of namespaces which should
    be whitelisted from deleting.
    
    If kube-system is not specified, it is added by default.
    
    Signed-off-by: M S Vishwanath Bhat <msvbhat@gmail.com>

--- main.go
+++ main.go
@@ -8,10 +8,31 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 	//"k8s.io/client-go/rest" -- To be Added when moving to in cluster setup
 	"log"
+	"os"
+	"strings"
 )
 
+// Get a list of whitelisted Namespaces. The pods in the whitelisted
+// will not be killed
+func getWhitelistedNS() (whitelistedNS []string) {
+	whitelistedNS = strings.Fields(os.Getenv("NAMESPACE_WHITELIST"))
+
+	// Append kube-system to list of whitelisted namespaces if not present
+	var flag = false
+	for _, ns := range whitelistedNS {
+		if "kube-system" == ns {
+			flag = true
+		}
+	}
+	if !flag {
+		whitelistedNS = append(whitelistedNS, "kube-system")
+	}
+	return whitelistedNS
+}
+
+// Get a list of all running pods in the cluster
 func getRunningPods(clientset *kubernetes.Clientset) (podsRunning []v1.Pod, err error) {
-	pods, err := clientset.CoreV1().Pods("default").List(metav1.ListOptions{})
+	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
 	if err != nil {
 		return nil, err
 	}
@@ -22,6 +43,30 @@ func getRunningPods(clientset *kubernetes.Clientset) (podsRunning []v1.Pod, err 
 	}
 	return podsRunning, nil
 
+}
+
+// Check if the pod belongs to any whitelisted Namespaces
+func isWhitelisted(whitelistedNS []string, pod v1.Pod) bool {
+	for _, ns := range whitelistedNS {
+		if pod.Namespace == ns {
+			return true
+		}
+	}
+	return false
+}
+
+// Get a list of all pods which are eligible for delete
+func getDeletablePods(clientset *kubernetes.Clientset, whitelistedNS []string) (deletablePods []v1.Pod, err error) {
+	runningPods, err := getRunningPods(clientset)
+	if err != nil {
+		return nil, err
+	}
+	for _, pod := range runningPods {
+		if !isWhitelisted(whitelistedNS, pod) {
+			deletablePods = append(deletablePods, pod)
+		}
+	}
+	return deletablePods, nil
 }
 
 func main() {
@@ -37,7 +82,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	deletablePods, err := getRunningPods(clientset)
+	whitelistedNS := getWhitelistedNS()
+	deletablePods, err := getDeletablePods(clientset, whitelistedNS)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -47,13 +93,13 @@ func main() {
 	}
 }
 
-// Initialise the clientset
 // Get the whitelisted namespaces
 // Get the running pods
 // Get the killable pods
 // Get the percentage of pods to kill
 // Kill random pods from the killable pods list
 // Wait for an 10 minutes and repeat the process
+// Refactor
 // Get a health endpoint
 // Get metric endpoint
 // Send events to pods
